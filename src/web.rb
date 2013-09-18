001f879575c946902ba67fb1522313f04e41a265
Daniel Hum <dan@oneuphero.com>
2013-09-18 18:07:06 +0800

    custom format for Scale Dynos

--- src/web.rb
+++ src/web.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'cgi'
 
 api_token = ENV['HIPCHAT_TOKEN']
 client = HipChat::Client.new(api_token)
@@ -15,7 +16,17 @@ post '/alert' do
   puts params
   payload = JSON.parse(params[:payload])
   puts payload
-  message = "%s: %s" % [payload['alert']['name'], payload['event']['m']]
-  puts message
-  client[room_id].send(username, message, color: 'red', notify: 1)
+  alert_name = payload['alert']['name']
+  if alert_name == "Scale Dynos"
+    message = payload['event']['m']
+    message = message.sub(/.*Scale/, 'Scale')
+    message = message.split('by').collect(&:strip)
+    message = CGI.escapeHTML("<b>#{message.first}</b> by <b>#{message.last}</b>")
+    puts message
+    client[room_id].send("Logentries/Heroku", message, color: 'purple', notify: 1)
+  else
+    message = "%s: %s" % [payload['alert']['name'], payload['event']['m']]
+    puts message
+    client[room_id].send(username, message, color: 'red', notify: 1)
+  end
 end
